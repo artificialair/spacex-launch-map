4cfc3d778369c74e057fa260ac02f5431d9c1f38
Zach <zach.Brewer.1995@gmail.com>
2023-11-30 22:03:16 -0500

    reformatted code & added text to card element

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,104 +5,113 @@ var launchCache;
 var cached = false;
 
 function displayCards(data, launchPadName, landingPadName) {
-    resultsContainer.innerHTML = null;
+  resultsContainer.innerHTML = null;
 
-    var cardEl = document.createElement('div');
-    cardEl.classList.add('card', 'col-mb-3', 'text-white', 'bg-secondary', 'mx-5');
-    cardEl.setAttribute('style', 'max-width: 28rem;');
-    
-    var cardBodyEl = document.createElement('div');
-    cardBodyEl.classList.add('card-body');
-    
-    var h4El = document.createElement('h4');
-    h4El.classList.add('card-title');
-    h4El.textContent = data.name;
-    
-    var pEl1 = document.createElement('p');
-    pEl1.classList.add('card-text');
-    pEl1.textContent = data.flight_number;
+  var cardEl = document.createElement("div");
+  cardEl.classList.add(
+    "card",
+    "col-md-3",
+    "text-white",
+    "bg-secondary",
+    "mx-4"
+  );
+  cardEl.setAttribute("style", "max-width: 28rem;");
 
-    var pEl2 = document.createElement('p');
-    pEl2.classList.add('card-text');
-    pEl2.textContent = data.date_utc;
+  var cardBodyEl = document.createElement("div");
+  cardBodyEl.classList.add("card-body");
 
-    var pEl3 = document.createElement('p');
-    pEl3.classList.add('card-text');
-    pEl3.textContent = launchPadName;
-    
-    var pEl4 = document.createElement('p');
-    pEl4.classList.add('card-text');
-    pEl4.textContent = landingPadName;
-    
-    var pEl5 = document.createElement('p');
-    pEl5.classList.add('card-text');
-    // pEl5.textContent = data.;
-    
-    var aEl = document.createElement('a');
-    aEl.classList.add('btn', 'btn-primary');
-    aEl.textContent = 'Youtube Link';
-    aEl.href = data.links.webcast;
-    aEl.target = "_blank";
-    resultsContainer
-        .appendChild(cardEl)
-        .appendChild(cardBodyEl)
-        .append(h4El, pEl1, pEl2, pEl3, pEl4, pEl5, aEl);
-};
+  var h4El = document.createElement("h4");
+  h4El.classList.add("card-title");
+  h4El.textContent = data.name;
+
+  var pEl1 = document.createElement("p");
+  pEl1.classList.add("card-text");
+  pEl1.textContent = "Flight Number: " + data.flight_number;
+
+  var pEl2 = document.createElement("p");
+  pEl2.classList.add("card-text");
+  pEl2.textContent = "Launch Date: " + data.date_utc;
+
+  var pEl3 = document.createElement("p");
+  pEl3.classList.add("card-text");
+  pEl3.textContent = "Launch Pad: " + launchPadName;
+
+  var pEl4 = document.createElement("p");
+  pEl4.classList.add("card-text");
+  pEl4.textContent = "Landing Pad: " + landingPadName;
+
+  var pEl5 = document.createElement("p");
+  pEl5.classList.add("card-text");
+  pEl5.textContent = "Payload: ";
+
+  var aEl = document.createElement("a");
+  aEl.classList.add("btn", "btn-primary");
+  aEl.textContent = "Youtube Link";
+  aEl.href = data.links.webcast;
+  aEl.target = "_blank";
+  
+  resultsContainer
+    .appendChild(cardEl)
+    .appendChild(cardBodyEl)
+    .append(h4El, pEl1, pEl2, pEl3, pEl4, pEl5, aEl);
+}
 
 function getPadData(result) {
-    var launchPadName;
-    var landingPadName;
+  var launchPadName;
+  var landingPadName;
 
-    fetch("https://api.spacexdata.com/v4/launchpads/" + result.launchpad)
-    .then(response => response.json())
-    .then(json => {
-        launchPadName = json.full_name
-    }).then(() => {
-        fetch("https://api.spacexdata.com/v4/landpads/" + result.cores[0].landpad)
-        .then(response => response.json())
-        .then(json => {
-            landingPadName = json.full_name
-            displayCards(result, launchPadName, landingPadName);
-        })
+  fetch("https://api.spacexdata.com/v4/launchpads/" + result.launchpad)
+    .then((response) => response.json())
+    .then((json) => {
+      launchPadName = json.full_name;
+    })
+    .then(() => {
+      fetch("https://api.spacexdata.com/v4/landpads/" + result.cores[0].landpad)
+        .then((response) => response.json())
+        .then((json) => {
+          landingPadName = json.full_name;
+          displayCards(result, launchPadName, landingPadName);
+        });
     });
-};
+}
 
 function searchLaunchCache(launchToSearch) {
-    for (var launch of launchCache) {
-        if (launch.flight_number == launchToSearch) {
-            var result = launch;
-            break;
-        }
+  for (var launch of launchCache) {
+    if (launch.flight_number == launchToSearch) {
+      var result = launch;
+      break;
     }
+  }
 
-    getPadData(result);
+  getPadData(result);
 }
 function createLaunchCache(launchToSearch) {
-    if (!cached) {
-        fetch("https://api.spacexdata.com/v4/launches")
-            .then(response => response.json())
-            .then(json => {
-                launchCache = json;
-                cached = true;
-                searchLaunchCache(launchToSearch);
-            })
-    } else {
+  if (!cached) {
+    fetch("https://api.spacexdata.com/v4/launches")
+      .then((response) => response.json())
+      .then((json) => {
+        launchCache = json;
+        cached = true;
         searchLaunchCache(launchToSearch);
-    }
+      });
+  } else {
+    searchLaunchCache(launchToSearch);
+  }
 }
 
 searchButton.addEventListener("click", function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    var launchToSearch = searchBar.value;
-    searchBar.value = null;
+  var launchToSearch = searchBar.value;
+  searchBar.value = null;
 
-    createLaunchCache(launchToSearch);
-})
+  createLaunchCache(launchToSearch);
+});
 
-var map = L.map('map').setView([51.505, -0.09], 14);
+var map = L.map("map").setView([51.505, -0.09], 14);
 
-L.tileLayer('https://tile.openstreetmap.org/0/0/0.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+L.tileLayer("https://tile.openstreetmap.org/0/0/0.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map);
